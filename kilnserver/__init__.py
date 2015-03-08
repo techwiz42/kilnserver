79511d2e83ad68d089d2382a1294ef7de745eac7
Robert Liesenfeld <xunil@xunil.net>
2015-03-08 11:36:04 -0700

    Removing unnecessary files and updating .gitignore.

--- kilnserver/__init__.py
+++ kilnserver/__init__.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 app.config.update(dict(
   SQLALCHEMY_DATABASE_URI='sqlite:///'+os.path.join(app.root_path, 'kilnserver.db'),
   DEBUG=True,
-  SECRET_KEY='099d77359a8c14d35c440b1589570f99',
-  CELERY_BROKER_URL='redis://localhost:6379/0'
+  SECRET_KEY='099d77359a8c14d35c440b1589570f99'
 ))
 app.config.from_envvar('KILNSERVER_SETTINGS', silent=True)
 
