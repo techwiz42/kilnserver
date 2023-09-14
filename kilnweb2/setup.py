257e46999ff536f9bdaffe4eda6b7f0f1e5154c9
techwiz42 <thetechwizard42@gmail.com>
2023-09-14 21:17:17 +0000

    removed obsolete data files, edited setup.py files and added requirements.txt

--- kilnweb2/setup.py
+++ kilnweb2/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name = 'kilnweb2',
     packages=['kilnweb2'],
+    version="0.9.0",
     include_package_data=True,
     install_requires=[
         'flask',
